4469b1bd1146e3c1da29c7a2ba93e0b829e77672
iktushar01 <iktushar2020@gmail.com>
2026-02-04 10:26:29 +0600

    Fixing Authentication Errors & Implementing Email Verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,41 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma.js";
+import { role } from "better-auth/plugins";
+import { request } from "node:http";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "user",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      }
+    },
+  },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token}, request) => {
+      console.log(`Send verification email to ${user.email} with url: ${url} and token: ${token}`);
+    }
   },
 });

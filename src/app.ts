4a6c04371373a0cc433b2c6c7669b94dc7ed9cfd
iktushar01 <iktushar2020@gmail.com>
2026-02-04 09:29:24 +0600

    intrigrate signup using beeter auth

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express, { Application } from "express"
 import cors from "cors"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app: Application = express()
+
+app.all("/api/auth/*spliat", toNodeHandler(auth));
 
 app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!")
+  res.status(200).json({
+    success: true,
+    message: "MedQuix Server is Running"
+  })
 })
 
 export { app }

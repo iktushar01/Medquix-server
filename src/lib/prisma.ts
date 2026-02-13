f008fe71aad2e50ff7b6a7342bf8ef4014c1d37b
iktushar01 <iktushar2020@gmail.com>
2026-02-13 15:19:40 +0600

    version change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,31 +1,20 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from '@neondatabase/serverless'
-import dotenv from 'dotenv'
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
-
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set. Please ensure it is defined in your environment variables.')
-}
-
-// Serverless-safe Neon pool
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    adapter,
-    log: ['query', 'info', 'warn', 'error'],
-  })
+    log: ["query", "info", "warn", "error"],
+  });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
